b8dc57ab4fad3902ed64ccac5e7171a66cb48e76
C2jay <cat.crane15@gmail.com>
2020-10-03 09:24:59 +0800

    categories

--- projects/models.py
+++ projects/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth import get_user_model
 
 class Project(models.Model):
     PROJECT_CATEGORIES = (  
-        ("Cupcakes", "Cupcakes"),  
-        ("Cake", "Cake"),  
-        ("Fondant", "Fondant"),  
-        ("Buttercream", "Buttercream"),  
-        ("Pastry", "Pastry"),
+        ("Cupcakes"),  
+        ("Cake"),  
+        ("Fondant"),  
+        ("Buttercream"),  
+        ("Pastry"),
         (None, "")
     )
 
