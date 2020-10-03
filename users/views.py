c724d53509b34c0c25abee97c02e3ccb5b8ab7c5
C2jay <cat.crane15@gmail.com>
2020-10-03 11:50:52 +0800

    user

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.parsers import MultiPartParser
 from .models import CustomUser
 from .serializers import CustomUserSerializer
@@ -59,14 +59,24 @@ class CustomUserDetail(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-class UserCreate(APIView):
-    def post(self, request, format='json'):
-        serializer = CustomUserSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            if user:
-                token = Token.objects.create(user=user)
-                json = serializer.data
-                json['token'] = token.key
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserCreate(generics.CreateAPIView):
+    # def post(self, request):
+    #     serializer = CustomUserSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         print('serializer is valid')
+    #         user = serializer.save()
+    #         if user:
+    #             print('user!')
+    #             token = Token.objects.create(user=user)
+    #             json = serializer.data
+    #             json['token'] = token.key
+    #             return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    def check_permissions(self, request):
+        if request.user.is_authenticated:
+            self.permission_denied(request)
+        
